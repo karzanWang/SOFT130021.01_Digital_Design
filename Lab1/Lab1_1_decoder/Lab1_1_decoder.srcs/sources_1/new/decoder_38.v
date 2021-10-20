b5d17a4cdb0e14eaa7ab0e0f5874b0795d23c6af
Wang Haiwei <haiweiwang1109@163.com>
2021-10-20 09:28:33 +0800

    lab1_part1

---
+++ Lab1/Lab1_1_decoder/Lab1_1_decoder.srcs/sources_1/new/decoder_38.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/10/20 09:18:54
+// Design Name: 
+// Module Name: decoder_38
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module decoder_38(
+        input [2:0] SW,
+        output [7:0] LED
+);
+decoder(SW, LED);
+endmodule
+
+module decoder(
+        input [2:0] a,
+        output [7:0] D
+    );
+ assign D[0] = (!a[0]) && (!a[1]) && (!a[2]);
+ assign D[1] = (!a[0]) && (!a[1]) && (a[2]);
+ assign D[2] = (!a[0]) && (a[1]) && (!a[2]);
+ assign D[3] = (!a[0]) && (a[1]) && (a[2]);
+ assign D[4] = (a[0]) && (!a[1]) && (!a[2]);
+ assign D[5] = (a[0]) && (!a[1]) && (a[2]);
+ assign D[6] = (a[0]) && (a[1]) && (!a[2]);
+ assign D[7] = (a[0]) && (a[1]) && (a[2]);
+ 
+endmodule
